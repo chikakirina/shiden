cea0c32025c1fa9423100004f97a3bd81f2edb7d
koutalou <k.koutalou@gmail.com>
2016-10-14 20:35:48 +0900

    :wrench: 構造変更

---
+++ Sumire/LeftMenu/LeftMenuCell.swift
@@ -0,0 +1,9 @@
+//
+//  LeftMenuCell.swift
+//  Sumire
+//
+//  Created by Kodama.Kotaro on 2016/10/14.
+//  Copyright © 2016年 koutalou. All rights reserved.
+//
+
+import Foundation
---
+++ Sumire/LeftMenu/LeftMenuViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LeftMenuViewController.swift
+//  Sumire
+//
+//  Created by Kodama.Kotaro on 2016/10/14.
+//  Copyright © 2016年 koutalou. All rights reserved.
+//
+
+import Foundation
